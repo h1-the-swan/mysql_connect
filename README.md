cb225c6d4f1c14bcd9275f1e934d6a007d576f97
hi-the-swan <jason.portenoy@gmail.com>
2018-05-21 14:52:07 -0700

    get_db_connection can't get db name from environment anymore

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Before importing, environment variables must be set:
 ```
 MYSQL_USERNAME="myusername"
 MYSQL_PASSWORD="mypassword"
-MYSQL_DB_NAME="dbname" (this one is optional. if it is set, it will be used to choose the database)
 ```
 
 One way to set these is to use the `python-dotenv` library (`pip install python-dotenv`) and load the variables from a `.env` file (which you do not commit to version control):
--- mysql_connect/db_connect.py
+++ mysql_connect/db_connect.py
@@ -8,15 +8,13 @@ database_settings = {
     'password': os.environ.get('MYSQL_PASSWORD'),
     'encoding': 'utf8mb4',
     'module_path': None,
-    'db_name': os.environ.get('MYSQL_DB_NAME')
 }
 
 def get_db_connection(db_name=None):
-    if database_settings['db_name'] is None:
-        if db_name is None:
-            raise RuntimeError("db_name not set")
-        else:
-            database_settings['db_name'] = db_name
+    if db_name is None:
+        raise RuntimeError("db_name not set")
+    else:
+        database_settings['db_name'] = db_name
     c = MySQLConnect(**database_settings)
     return c
 
